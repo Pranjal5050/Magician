0d7085282130fa9a919d83186cd5eb4b81f78dcc
Pranjal Sharma <pranjalsharma9143@gmail.com>
2026-06-19 14:54:11 +0530

    Setup Brevo SMTP

--- Backend/services/mail.service.js
+++ Backend/services/mail.service.js
@@ -1,22 +1,13 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("Mail Error:", error);
-  } else {
-    console.log("Mail Server Ready");
-  }
 });
 
 module.exports = transporter;

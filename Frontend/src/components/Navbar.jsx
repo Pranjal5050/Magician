b48ec707eb300987d0faa76a6b16cdcda726254b
Pranjal Sharma <pranjalsharma9143@gmail.com>
2026-06-20 14:06:59 +0530

    Fix Navbar logo

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           {/* Logo */}
           <div className="flex items-center justify-center">
             <img
-              className="h-20 md:h-50 object-cover"
+              className="h-20 md:h-50 "
               src="/logo.png"
               alt="logo"
             />
